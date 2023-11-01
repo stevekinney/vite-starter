518c5d2013ea50cce53e40fbde5bba586d920423
Steve Kinney <hello@stevekinney.net>
2023-11-01 15:02:22 -0500

    Render characters to test

--- src/characters/render-character.ts
+++ src/characters/render-character.ts
@@ -1,6 +1,6 @@
 import type { Character } from './generate-character';
 
-const createCharacterElement = (character: Character): HTMLElement => {
+export const renderCharacterElement = (character: Character): HTMLElement => {
   const characterDiv = document.createElement('div');
   characterDiv.className = 'character-card';
 
@@ -43,4 +43,22 @@ const createCharacterElement = (character: Character): HTMLElement => {
   return characterDiv;
 };
 
-export default createCharacterElement;
+export function renderCharacter(character: Character): string {
+  let result = '';
+
+  result += `Name: ${character.name}\n`;
+  result += `Class: ${character.characterClass}\n`;
+  result += `Strength: ${character.strength}\n`;
+  result += `Dexterity: ${character.dexterity}\n`;
+  result += `Wisdom: ${character.wisdom}\n`;
+  result += `Intelligence: ${character.intelligence}\n`;
+  result += `Constitution: ${character.constitution}\n`;
+  result += `Hit Points: ${character.hitPoints}\n`;
+  result += `Mana: ${character.mana}\n`;
+  result += `Fun Fact: ${character.funFact}\n`;
+  result += `Items: ${character.items.join(', ')}\n`;
+
+  return result;
+}
+
+export default renderCharacter;
